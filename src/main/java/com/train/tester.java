1c376e5905a52381961d28c115ba8342ec9184c9
swift <jms632068@gmail.com>
2019-08-11 01:31:46 +0800

    homework two together with homework one

--- src/main/java/com/train/tester.java
+++ src/main/java/com/train/tester.java
@@ -4,13 +4,21 @@ import java.util.Scanner;
 
 public class tester {
     public static void main(String[] args) {
-        System.out.print("Please enter number of tickets: ");
-        Scanner scanner=new Scanner(System.in);
-        int total=scanner.nextInt();
-        System.out.print("How many round-trip tickets: ");
-        int round=scanner.nextInt();
+        int total = 0;
+        int endNumber = -1;
 
-        Ticket tk=new Ticket(total,round);
-        tk.print();
+        while(total != endNumber) {
+            System.out.print("Please enter number of tickets: ");
+            Scanner scanner = new Scanner(System.in);
+            total = scanner.nextInt();
+            if(total == endNumber) break;
+            System.out.print("How many round-trip tickets: ");
+            int round = scanner.nextInt();
+            if(round == endNumber) break;
+
+            Ticket tk = new Ticket(total, round);
+            tk.print();
+            System.out.println("\n------Enter '-1' to exit------\n");
+        }
     }
 }
--- src/main/kotlin/com/train/TestKotlin.kt
+++ src/main/kotlin/com/train/TestKotlin.kt
@@ -3,21 +3,29 @@ package com.train
 import java.util.*
 
 fun main(args: Array<String>) {
-    print("Please enter number of tickets: ")
-    val scanner:Scanner= Scanner(System.`in`)
-    var total=scanner.nextInt()
-    print("How many round-trip tickets: ")
-    var round=scanner.nextInt()
+    val endNumber = -1
+    var total = 0
 
-    val tk:TrainTicket= TrainTicket(total,round)
-    tk.print()
+    while(total != endNumber) {
+        print("Please enter number of tickets: ")
+        val scanner: Scanner = Scanner(System.`in`)
+        total = scanner.nextInt()
+        if(total == endNumber) break
+        print("How many round-trip tickets: ")
+        var round = scanner.nextInt()
+        if(round == endNumber) break
+
+        val tk: TrainTicket = TrainTicket(total, round)
+        tk.print()
+        println("\n------Enter '-1' to exit------\n");
+    }
 }
 
-class TrainTicket(var total:Int,var round:Int){
-    fun print(){
-        var singlePrize=(total-round)*1000
-        var roundPrize=(round*2000)*9/10
-        var sum=singlePrize+roundPrize
+class TrainTicket(var total:Int,var round:Int) {
+    fun print() {
+        var singlePrize = (total - round) * 1000
+        var roundPrize = (round * 2000) * 9 / 10
+        var sum = singlePrize + roundPrize
         println("Total tickets: $total")
         println("Round-trip: $round")
         println("Total: $sum")
